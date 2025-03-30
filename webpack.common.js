439e979de4f3136117a921668dfedef6d3453787
ndgndg91 <ndgndg91@gmail.com>
2025-03-30 23:51:51 +0900

    jvm warmup blogging

--- webpack.common.js
+++ webpack.common.js
@@ -83,6 +83,14 @@ module.exports = {
     }),
     new HtmlWebpackPlugin({
       template: './layouts/main.hbs',
+      filename: "blog/software-engineer/list/jvm-warmup.html",
+      chunks: ['index'],
+      templateParameters: {
+        content: require('fs').readFileSync(path.resolve(__dirname, 'blog/software-engineer/list/jvm-warmup.hbs'), 'utf8'),
+      }
+    }),
+    new HtmlWebpackPlugin({
+      template: './layouts/main.hbs',
       filename: "blog/software-engineer/list/kafka-basic.html",
       chunks: ['index'],
       templateParameters: {
